3803fc129f91cf73fc246ba242acf3d33fcf6259
Shuvrojit Biswas <jsuvro17@gmail.com>
2023-05-22 20:01:41 +0600

    Change admin view for devicelog

--- trackr/admin.py
+++ trackr/admin.py
@@ -5,4 +5,10 @@ from .models import Company, Employee, Device, DeviceLog
 admin.site.register(Company)
 admin.site.register(Employee)
 admin.site.register(Device)
-admin.site.register(DeviceLog)
+
+@admin.register(DeviceLog)
+class DeviceLogAdmin(admin.ModelAdmin):
+
+    list_display = ("device", "employee", "checkout_time", "return_time", "condition")
+    list_filter = ("device", "employee", "checkout_time", "return_time", "condition")
+    list_editable = ("return_time", "condition")
